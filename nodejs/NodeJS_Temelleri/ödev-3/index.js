222d34210859998dee18c5efe84c5829f9125801
umutbozbek <umutbozbek58@gmail.com>
2022-05-25 21:42:58 +0300

    ödev-3

---
+++ nodejs/NodeJS_Temelleri/ödev-3/index.js
@@ -0,0 +1,8 @@
+
+// require ve object destructing kullanarak index.js 
+// dosyasında yarıçap (r) 5 olacak şekilde ekran çıktısını alınız.v
+
+
+const {circleArea} =require('./circle')
+
+circleArea(5)
